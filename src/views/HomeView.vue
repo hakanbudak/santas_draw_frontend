b5b8311aeb4a88d69f0a409dfbee110dd8cc5003
Hakan Budak <hakanbudk0@gmail.com>
2025-12-08 15:43:18 +0300

    Feature: Producthunt popup added, changed to mobile size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="min-h-screen flex flex-col lg:flex-row overflow-hidden relative">
+
+    <div class="absolute top-4 left-4 z-50">
+      <a href="https://www.producthunt.com/products/lets-raffle?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-lets&#0045;raffle" target="_blank">
+        <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1047397&theme=light&t=1765196820214" alt="Lets&#0032;Raffle - Organize&#0032;Secret&#0032;Santa&#0032;exchanges&#0046;&#0032;Fair&#0044;&#0032;fun&#0044;&#0032;festive&#0033; | Product Hunt" style="width: 150px; height: 32.4px;" class="md:!w-[250px] md:!h-[54px]" width="150" height="32.4" />
+      </a>
+    </div>
+
     <div class="snowfall"></div>
     <div class="hidden lg:flex w-full lg:w-1/2 bg-gradient-to-br from-red-600 via-red-700 to-red-800 items-center justify-center relative overflow-hidden">
       <div class="absolute inset-0 bg-[radial-gradient(circle_at_30%_20%,rgba(255,255,255,0.1),transparent)]"></div>
